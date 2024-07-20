26b46a424cc1c889715582c37b830ddb50f91980
Aditya-Joshi <joshiaditya.aj07@gmail.com>
2024-07-20 12:32:46 +0530

    feat: added blog schemas and refactor

--- common/src/index.ts
+++ common/src/index.ts
@@ -8,13 +8,8 @@ export const signInBodySchema = z.object({
     .max(8, { message: "Must be 8 or fewer characters long" }),
 });
 
-export type SigninType = z.infer<typeof signInBodySchema>;
-
 export const signUpBodySchema = z.object({
-  name: z.string({
-    required_error: "Name is required",
-    invalid_type_error: "Name must be a string",
-  }),
+  name: z.string({message: "Name is required"}),
   email: z.string().email({ message: "Invalid email address" }),
   password: z
     .string()
@@ -22,11 +17,41 @@ export const signUpBodySchema = z.object({
     .max(8, { message: "Password must be 8 or fewer characters long" }),
 });
 
-export type SignupType = z.infer<typeof signUpBodySchema>;
-
 export const verifyCodeSchema = z.object({
-  email: z.string().email(),
+  userId: z.string(),
   code: z.string().length(6, "Verification code must be 6 digits."),
 });
 
+export const CreateBlogSchema = z.object({
+  title: z
+    .string()
+    .min(3, "Title must be greater than 3 characters.")
+    .max(256, "Title must be lesser than 256 characters."),
+  content: z.any(),
+  published: z.boolean().default(false),
+});
+
+export const UpdateBlogSchema = z.object({
+  title: z
+    .string()
+    .min(3, "Title must be greater than 3 characters.")
+    .max(256, "Title must be lesser than 256 characters."),
+  content: z.any(),
+  published: z.boolean().default(false),
+});
+
+export const QuerySchema = z.object({
+  page: z.coerce.number().int().positive().default(1),
+  pageSize: z.coerce.number().int().positive().max(100).default(10),
+  sortBy: z.enum(["publishedAt", "title"]).default("publishedAt"),
+  order: z.enum(["asc", "desc"]).default("desc"),
+  author: z.string().optional(),
+});
+
+export type SigninType = z.infer<typeof signInBodySchema>;
+export type SignupType = z.infer<typeof signUpBodySchema>;
 export type verifyCodeType = z.infer<typeof verifyCodeSchema>;
+
+export type CreateBlogType = z.infer<typeof CreateBlogSchema>;
+export type UpdateBlogType = z.infer<typeof UpdateBlogSchema>;
+export type QueryType = z.infer<typeof QuerySchema>;
