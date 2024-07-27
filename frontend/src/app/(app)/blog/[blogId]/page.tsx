54749e60f6fe21afdddad31c333d93b2e6b1f08f
Aditya-Joshi <joshiaditya.aj07@gmail.com>
2024-07-28 04:27:16 +0530

    fix: probable fix for hydration errors

--- frontend/src/app/(app)/blog/[blogId]/page.tsx
+++ frontend/src/app/(app)/blog/[blogId]/page.tsx
@@ -17,16 +17,17 @@ const Blog: FC<BlogProps> = async ({
 }) => {
   const data = await blogService.getBlogById(params.blogId);
   const formattedDate = format(new Date(data.blog.publishedAt), "dd/MM/yyyy");
+  console.log("Formatted Date in blog page", formattedDate);
+  console.log("Date from blog in blog page", data.blog.publishedAt);
 
   // console.log(data);
   return (
     <div className="container mx-auto px-0 py-2 md:px-2 md:py-4 lg:px-4 lg:py-8">
       <h1 className="text-4xl font-bold mb-4">{data.blog.title}</h1>
-      <p className="text-gray-600 mb-4">
-        {data.blog.author?.name} ⟡ {" "}
-        {formattedDate}
+      <p className="text-gray-600 mb-4" suppressHydrationWarning={true}>
+        {data.blog.author?.name} ⟡ {formattedDate}
       </p>
-      <Separator className="text-white/100 mb-4"/>
+      <Separator className="text-white/100 mb-4" />
       <BlogContent content={data.blog.content} />
     </div>
   );
